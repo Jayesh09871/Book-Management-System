02b5d08c288e1d5313413d8e609494d2d4f10e3f
Jayesh09871 <jayeshkakhani29@gmail.com>
2024-11-06 11:18:53 +0530

    updated

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -13,45 +13,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-    phone: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      validate: {
-        validator: function (v) {
-          return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(v);
-        },
-        message: (props) => `${props.value} is not a valid email!`,
-      },
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 8,
-      maxlength: 15,
-      trim: true,
-    },
-    address: {
-      street: {
-        type: String,
-        trim: true,
-      },
-      city: {
-        type: String,
-        trim: true,
-      },
-      pincode: {
-        type: String,
-        trim: true,
-      },
-    },
+    phone:{
+      type: Number,
+      
+    }
+    
   },
   { timestamps: true } 
 );
